ef0fc46bbcbd47ee35a33a685288adba9bd15442
@juanwmedia <juan@wmedia.es>
2016-06-23 20:11:00 +0200

    Primer commit

---
+++ www/js/controllers.js
@@ -0,0 +1,57 @@
+function ChatController($scope, $firebaseArray, $firebaseAuth) {
+    $scope.shouldShowDelete = false;
+    $scope.shouldShowReorder = false;
+    $scope.listCanSwipe = true;
+    $scope.nuevoMensaje = '';
+    $scope.autenticado = false;
+    $scope.usuario = null;
+
+    // Autentifiación
+    var auth = $firebaseAuth();
+
+    $scope.conectar = function() {
+        auth.$signInWithPopup("google").then(function(firebaseUser) {
+        }).catch(function(error) {
+            console.log("Authentication failed:", error);
+        });
+    };
+
+    $scope.desconectar = function() {
+        auth.$signOut();
+    }
+
+    auth.$onAuthStateChanged(function(firebaseUser) {
+        if (firebaseUser) {
+            console.log("Signed in as:", firebaseUser);
+            $scope.autenticado = true;
+            $scope.usuario = firebaseUser;
+        } else {
+            console.log("Signed out");
+            $scope.autenticado = false;
+            $scope.usuario = null;
+        }
+    });
+
+    // Real-time database
+    var referencia = firebase.database().ref().child("mensajes");
+    $scope.chats = $firebaseArray(referencia);
+
+    $scope.enviarMensaje = function() {
+        $scope.chats.$add({
+            mensaje: $scope.nuevoMensaje,
+            usuario: $scope.usuario.displayName,
+            avatar: $scope.usuario.photoURL,
+            uid: $scope.usuario.uid
+        });
+
+        $scope.nuevoMensaje = '';
+    };
+
+    $scope.borrarMensaje = function(mensaje) {
+        $scope.chats.$remove(mensaje);
+    };
+}
+
+angular
+    .module('chatID3')
+    .controller('ChatController', ChatController);
